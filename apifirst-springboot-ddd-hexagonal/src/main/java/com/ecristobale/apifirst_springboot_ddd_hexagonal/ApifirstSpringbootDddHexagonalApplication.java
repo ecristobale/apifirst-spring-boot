884405e1d1f4eb6531b9a04455ab7c0e39569527
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-04-04 20:29:01 +0200

    create spring boot project

---
+++ apifirst-springboot-ddd-hexagonal/src/main/java/com/ecristobale/apifirst_springboot_ddd_hexagonal/ApifirstSpringbootDddHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.ecristobale.apifirst_springboot_ddd_hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstSpringbootDddHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifirstSpringbootDddHexagonalApplication.class, args);
+	}
+
+}
