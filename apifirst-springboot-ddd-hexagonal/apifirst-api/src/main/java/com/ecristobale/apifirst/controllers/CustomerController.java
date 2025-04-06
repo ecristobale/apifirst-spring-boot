e115310afd1501325d41dc4a7caae2c1a4f835b8
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-04-06 22:43:39 +0200

    Created endpoint for getting one customer by id

--- apifirst-springboot-ddd-hexagonal/apifirst-api/src/main/java/com/ecristobale/apifirst/controllers/CustomerController.java
+++ apifirst-springboot-ddd-hexagonal/apifirst-api/src/main/java/com/ecristobale/apifirst/controllers/CustomerController.java
@@ -5,10 +5,12 @@ import com.ecristobale.apifirst.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 import static com.ecristobale.apifirst.controllers.CustomerController.BASE_PATH;
 
@@ -24,5 +26,10 @@ public class CustomerController {
     @GetMapping
     public ResponseEntity<List<Customer>> listCustomers() {
         return ResponseEntity.ok(customerService.listCustomers());
+    }
+
+    @GetMapping("{customerId}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
     }
 }
--- apifirst-springboot-ddd-hexagonal/apifirst-api/src/main/java/com/ecristobale/apifirst/services/CustomerService.java
+++ apifirst-springboot-ddd-hexagonal/apifirst-api/src/main/java/com/ecristobale/apifirst/services/CustomerService.java
@@ -3,7 +3,10 @@ package com.ecristobale.apifirst.services;
 import com.ecristobale.apifirst.model.Customer;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<Customer> listCustomers();
+
+    Customer getCustomerById(UUID customerId);
 }
