3d74dbc3eb71cd0cc55e443b617ab77d9232216f
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-04-06 23:21:03 +0200

    created test for list customers

---
+++ apifirst-springboot-ddd-hexagonal/apifirst-api/src/test/java/com/ecristobale/apifirst/controllers/BaseTest.java
@@ -0,0 +1,30 @@
+package com.ecristobale.apifirst.controllers;
+
+import com.ecristobale.apifirst.repositories.CustomerRepository;
+import com.ecristobale.apifirst.repositories.OrderRepository;
+import com.ecristobale.apifirst.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+public class BaseTest {
+
+    @Autowired
+    CustomerRepository customerRepository;
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    OrderRepository orderRepository;
+    @Autowired
+    WebApplicationContext wac;
+
+    public MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+}
---
+++ apifirst-springboot-ddd-hexagonal/apifirst-api/src/test/java/com/ecristobale/apifirst/controllers/CustomerControllerTest.java
@@ -0,0 +1,24 @@
+package com.ecristobale.apifirst.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class CustomerControllerTest extends BaseTest {
+
+    @DisplayName("Test List Customers")
+    @Test
+    void TestListCustomers() throws Exception {
+        mockMvc.perform(get(CustomerController.BASE_PATH)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()", greaterThan(0)));
+    }
+}
