e38b7b63a260d146fef510161971022c1ee60a15
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-04-23 15:47:01 +0200

    Refactor open api model properties

--- apifirst-sb/apifirst-springboot/src/main/java/com/ecristobale/apifirst/apifirstspringboot/repositories/CustomerRepository.java
+++ apifirst-sb/apifirst-springboot/src/main/java/com/ecristobale/apifirst/apifirstspringboot/repositories/CustomerRepository.java
@@ -2,7 +2,6 @@ package com.ecristobale.apifirst.apifirstspringboot.repositories;
 
 import com.ecristobale.apifirst.apifirstspringboot.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
